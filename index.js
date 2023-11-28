63b36c6a244dc3338fb0678ef20afcb88a9e2d06
daniel <adriandanielfernandez@gmail.com>
2023-11-28 17:43:10 -0300

    iniciando

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+
+const app = express();
+
+
+app.listen(3000,()=>{
+    console.log("server listening on 3000");
+}) 
---
+++ model.js
@@ -0,0 +1,6 @@
+import prisma from "@prisma/client"
+
+
+
+  
+ 
